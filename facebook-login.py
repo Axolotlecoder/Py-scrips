1bb01caee49e1bf4c6728cc4af9ff0b48e6c8ba6
Axolotlecoder <151933458+Axolotlecoder@users.noreply.github.com>
2025-05-31 13:38:44 +0200

    Create facebook-login.py

---
+++ facebook-login.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#pip install selenium
+import getpass
+
+from selenium import webdriver
+
+PATH = "/usr/bin/google-chrome"  # change your path
+
+user = input("UserName:")
+passwd = getpass.getpass("Password:", stream=None)
+
+browser = webdriver.Chrome(executable_path=PATH)
+
+website_URL = "https://www.facebook.com/"
+browser.get(website_URL)
+
+element_email = browser.find_element_by_id("email")
+element_email.clear()
+element_email.send_keys(user)
+
+element_password = browser.find_element_by_id("pass")
+element_password.clear()
+element_password.send_keys(passwd)
+
+element_login_button = browser.find_element_by_id("u_0_b").click()
